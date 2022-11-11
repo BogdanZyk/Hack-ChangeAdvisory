9537e7021ca17a97ba20d2c2ad4f877e1afee36c
Bogdan Zykov <b.zykov@brightlab.me>
2022-11-12 01:02:07 +0300

    Initial Commit

---
+++ Hack&ChangeAdvisory/Hack_ChangeAdvisoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hack_ChangeAdvisoryApp.swift
+//  Hack&ChangeAdvisory
+//
+//  Created by Bogdan Zykov on 12.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Hack_ChangeAdvisoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
